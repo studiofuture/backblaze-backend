7cfe20dc7ae0eb60b6adb0587916f6996bd89a82
studiofuture <guy@studiofuture.co>
2025-06-17 16:08:44 +0100

    Update upload.js

--- routes/upload.js
+++ routes/upload.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const fs = require('fs');
+const busboy = require('busboy');
 const { 
   initUploadStatus, 
   updateUploadStatus, 
